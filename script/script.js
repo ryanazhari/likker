10d8e997b4094d60eacc69734a87b73c20669879
Ryan Azhari <ryanazhari.222@gmail.com>
2020-06-23 00:18:40 +0700

    First Upload

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const sideNav = $('.sidenav');
+M.Sidenav.init(sideNav, {
+    edge : 'right'
+});
+
+const tabs = $('.tabs');
+M.Tabs.init(tabs);
+// $(document).ready(function(){
+//     $('.tabs').tabs();
+// });
